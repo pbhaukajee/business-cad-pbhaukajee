4c799c2552348854360f17e2829582fd0a70bad7
Priyanka <pbhaukajee@gmail.com>
2024-07-17 17:43:37 -0500

    complete main section

---
+++ vite-react/src/components/Main.jsx
@@ -0,0 +1,20 @@
+export default function Main() {
+  return (
+    <div className="main-content">
+      <h2>About</h2>
+      <p>
+        My dedication and perseverance toward the journey of coding has given me
+        the strength to believe in myself. Currently, I am working on sharpening
+        my developer skills by learning and applying the knowledge of popular
+        frameworks like React which will help me build exicting and unique
+        projects.
+      </p>
+      <h2>Interests</h2>
+      <p>
+        Apart from coding, I enjoy cooking as well as spending time with my
+        family. Also, I love travelling to different places to enjoy new
+        cultures and traditions.
+      </p>
+    </div>
+  );
+}
