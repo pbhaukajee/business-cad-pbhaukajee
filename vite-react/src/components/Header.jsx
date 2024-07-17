f616226d39853eb9419ae3f6db546ff78b81cfcf
Priyanka <pbhaukajee@gmail.com>
2024-07-17 17:35:47 -0500

    add header section

--- vite-react/src/components/Header.jsx
+++ vite-react/src/components/Header.jsx
@@ -1,14 +1,31 @@
 import Pri from "../images/pri.png";
+import Email from "../images/email.png";
+import Linkedin from "../images/linkedin.png";
 
 export default function Header() {
   return (
     <div>
       <img className="profile-image" src={Pri} alt="Profile Image" />
-      <h1>Priyanka Bhaukajee</h1>
-      <h3>React Developer</h3>
-      <p>pbhaukajee@gmail.com</p>
-      <button>Email</button>
-      <button>Linkedin</button>
+      <div className="header--content">
+        <h1>Priyanka Bhaukajee</h1>
+        <h3>React Developer</h3>
+        <p className="header--email">pbhaukajee@gmail.com</p>
+        <div className="header--button">
+          <button>
+            <span>
+              <img className="icon" src={Email} alt="email" />
+            </span>
+            Email
+          </button>
+
+          <button className="button-2">
+            <span>
+              <img className="icon icon-linkedin" src={Linkedin} alt="email" />
+            </span>
+            Linkedin
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
